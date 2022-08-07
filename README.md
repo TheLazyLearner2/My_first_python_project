493dd68732c3f32ed6bd20b3b90248ef81ec9efd
Edward A Polanco Murillo <epolancomurillo@gmail.com>
2022-08-07 01:06:12 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Password Generator
+A simple Python script that creates random passwords using letters, numbers & special characters.
+
+# Uses
+Creates random or customized passwords for users. It helps users create stronger passwords that provide greater security. 
+
+# Setup
+You need `python>3.8` to run this script.
+This script uses both the `random` and `string` modules.
+
+# How it runs
+Script can be run in the command-line using ```python password_generator.py```
+Once it runs it will produce the random generated password. The length can be between 8 & 20 characters long. Lastly, After the password has been generated it will be stored with a text file(```used_passwords.txt```).
+
+# Side Note
+This is my very first project with Python. I know there are probably dozens of ways to make this script more efficient but, You have to start somewhere.
+
+
+
---
+++ password_gen.py
@@ -0,0 +1,51 @@
+import random
+import string
+
+def password(passwd_length=int):
+    print('WELCOME TO PASSWORD GENERATOR.')
+    print('Password must be between 8 and 20 characteres long.\n')
+    
+    letters = string.ascii_letters
+    numbers = string.digits
+    special_char = string.punctuation
+    
+    all_symbols = letters + numbers + special_char
+    
+
+    while range(passwd_length):
+        if passwd_length >= 8 and passwd_length <= 20:
+            generated_password = random.sample(all_symbols, passwd_length)
+            comb_password = "".join(generated_password)
+            print('Your Password is', comb_password)
+            return comb_password
+        else:
+            print('Password length cannot be less than 8 characters or more than 20 characters. Please try again')
+            return None
+        
+    
+def store_used_password(password):
+    file = password + '\n'
+    with open("used_passwords.txt", "a") as f:
+        for i in file:
+            f.write(i)
+                      
+                     
+new_password = password(10)
+store_used_password(new_password)
+
+
+
+
+
+
+
+
+
+'''file = open("used_passwords.txt", "w")
+file.write(password)
+file.close()
+f.write(str(i) + '\n')'''
+    
+    
+    
+        
